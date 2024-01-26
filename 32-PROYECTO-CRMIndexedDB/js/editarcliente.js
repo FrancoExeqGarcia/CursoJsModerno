51eeb7e9e0f9803de40244f745d723348169cb6c
Franco Garcia <francoexequiel.garcia150@gmail.com>
2024-01-26 13:38:02 -0300

    Cambios en keypath

--- 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -2,30 +2,28 @@
 
     let DB;
     let idCliente;
-    const formulario = document.querySelector('#formulario');
    
     const nombreInput = document.querySelector('#nombre');
     const emailInput = document.querySelector('#email');
-    const empresaInput = document.querySelector('#empresa');
     const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
-
-
         conectarDB();
 
         //
         formulario.addEventListener('submit', actualizarCliente);
 
 
-        // Verificar si el cliente existe
+        // Verificar ID
         const parametrosURL = new URLSearchParams(window.location.search);
         idCliente = parametrosURL.get('id');
         if(idCliente) {
    
-            setTimeout( () => {
+            setTimeout(() => {
                 obtenerCliente(idCliente);
-            }, 100);
+            }, 1000);
         }
 
     });
@@ -45,6 +43,7 @@
         abrirConexion.onsuccess = function() {
             // guardamos el resultado
             DB = abrirConexion.result;
+            console.log('Exito al abrir la base de datos');
         };
     }
 
@@ -56,11 +55,11 @@
 
         console.log(objectStore);
 
-        var request = objectStore.openCursor();
-        request.onsuccess = function(event) {
-            var cursor = event.target.result;
+        const cliente = objectStore.openCursor();
+        cliente.onsuccess = function(event) {
+            const cursor = event.target.result;
             if (cursor) {
-                if(cursor.value.id  == id ) {
+                if(cursor.value.id  === Number(id) ) {
                     // pasar el que estamos editando...
                     llenarFormulario(cursor.value);
                 }
@@ -71,11 +70,11 @@
     }
 
     function llenarFormulario(datosCliente) {
-        const { nombre, email, empresa, telefono } = datosCliente;
+        const { nombre, email, telefono, empresa } = datosCliente;
          nombreInput.value = nombre;
          emailInput.value = email;
-         empresaInput.value = empresa;
          telefonoInput.value = telefono;
+         empresaInput.value = empresa;
     }
 
     function actualizarCliente(e) {
@@ -103,6 +102,7 @@
         const objectStore = transaction.objectStore('crm');
 
         objectStore.put(clienteActualizado);
+        console.log(transaction);
 
         transaction.oncomplete = () => {
             imprimirAlerta('Editado Correctamente');
@@ -112,9 +112,9 @@
             }, 3000);
         };
 
-        transaction.onerror = (error) => {
-            console.log(error);
-            console.log('Hubo un errorr.');
+        transaction.onerror = function(error) {
+            imprimirAlerta('Hubo un error','error');
+            console.log('error al actualizar ccliente');
         };
     }
 
--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -4,16 +4,17 @@
     const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
-        formulario.addEventListener('submit', validarCliente);
-
         conectarDB();
+        
+        formulario.addEventListener('submit', validarCliente);
     });
+
 
     function conectarDB() {
         // ABRIR CONEXIÓN EN LA BD:
-
-        let abrirConexion = window.indexedDB.open('crm', 1);
-
+    
+        const abrirConexion = window.indexedDB.open('crm', 1);
+    
         // si hay un error, lanzarlo
         abrirConexion.onerror = function() {
             console.log('Hubo un error');
@@ -25,7 +26,34 @@
             DB = abrirConexion.result;
         };
     }
-
+    
+    function imprimirAlerta(mensaje, tipo) {
+             
+        const alerta = document.querySelector('.alerta');
+        
+        if(!alerta){
+        // Crea el div
+         const divMensaje = document.createElement('div');
+         divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center", "alerta" );
+    
+         if(tipo === 'error') {
+            divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
+         } else {
+             divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
+         }
+         
+         // Mensaje de error
+         divMensaje.textContent = mensaje;
+    
+         // Insertar en el DOM
+        formulario.appendChild(divMensaje);
+    
+         // Quitar el alert despues de 3 segundos
+         setTimeout( () => {
+             divMensaje.remove();
+         }, 3000);
+        }
+    }
 
     function validarCliente(e) {
         e.preventDefault();
@@ -38,6 +66,7 @@
 
         if(nombre === '' || email === '' || telefono === '' || empresa === '') {
              
+            imprimirAlerta('todos los campos son obligatorios','error');
 
             return;
         }
@@ -56,13 +85,10 @@
         cliente.id = Date.now();
 
 
-
         crearNuevoCliente(cliente);
     }
 
     function crearNuevoCliente(cliente) {
-
-        
 
         // NUEVO: 
         const transaction = DB.transaction(['crm'], 'readwrite');
@@ -87,28 +113,6 @@
         };
     }
 
-    function imprimirAlerta(mensaje, tipo) {
-         // Crea el div
 
-         const divMensaje = document.createElement('div');
-         divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center" );
-
-         if(tipo === 'error') {
-            divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
-         } else {
-             divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
-         }
-         
-         // Mensaje de error
-         divMensaje.textContent = mensaje;
- 
-         // Insertar en el DOM
-        formulario.appendChild(divMensaje);
- 
-         // Quitar el alert despues de 3 segundos
-         setTimeout( () => {
-             divMensaje.remove();
-         }, 3000);
-    }
 
 })();
