349630522ff0acac09c096df52550cd85bafb1dc
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2024-01-29 18:41:13 -0300

    fetch api

---
+++ 35-FetchAPI/js/01-app.js
@@ -0,0 +1,30 @@
+// api desde txt
+
+const cargarTxtBtn = document.querySelector('#cargarTxt');
+
+cargarTxtBtn.addEventListener('click', obtenerDatos);
+
+
+function obtenerDatos() {
+    const url = 'data/datos.txt' //url
+    fetch(url) 
+        .then( respuesta => {
+            console.log(respuesta);
+
+            console.log(respuesta.headers.get("Content-Type"));
+            console.log(respuesta.status); // Estado
+            console.log(respuesta.statusText); // STATE
+            console.log(respuesta.url); // URL que se consulta
+            console.log(respuesta.type); // consulta
+
+
+            return respuesta.text(); 
+
+        })
+        .then( datos => {
+            console.log(datos);
+        })
+        .catch( error => {
+            console.log(error);
+        })
+}
